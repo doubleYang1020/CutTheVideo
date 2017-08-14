1e37772a8ff93f5f173fc5c3358c70afc768229f
阳眼的熊 <237505990@qq.com>
2017-08-14 11:14:10 +0800

    Initial Commit

---
+++ CutTheVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CutTheVideo
+//
+//  Created by huyangyang on 2017/8/14.
+//  Copyright © 2017年 huyangyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
