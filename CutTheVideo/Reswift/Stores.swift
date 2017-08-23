1c9d5e893a747ef18ce153b478d79257e4eba096
Pikaurd <pikaurd@gmail.com>
2017-08-23 15:55:19 +0800

    换方案

--- CutTheVideo/Reswift/Stores.swift
+++ CutTheVideo/Reswift/Stores.swift
@@ -12,4 +12,19 @@ import ReSwift
 struct AppState: StateType {
   var undoHistory = UndoHistory()
   var videoSegmentSequence: [VideoSegment] = []
+  var processingCount: Int = -1
+}
+
+extension Integer {
+  func leftPad(expectedLength: Int) -> String {
+    guard expectedLength > 1 else { return self.description }
+    let natureLength = self.description.characters.count
+    let padLength = expectedLength - natureLength
+    if padLength > 0 {
+      let x = (0..<padLength).reduce("", { $0.0 + "0" })
+      return x + self.description
+    }
+    return self.description
+  }
+  
 }
